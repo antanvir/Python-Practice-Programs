7a41c1fb9e61a795905b0facdc8f95006497d603
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2018-12-29 23:04:49 +0600

    Update login.py

--- LogIn_SignUp_Form/login.py
+++ LogIn_SignUp_Form/login.py
@@ -7,34 +7,53 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from signup import Ui_SIGNUPFORM
+#import signup as file
 
-def signUpCheck():
-     print("Sign Up Button Clicked.")
+
+
 
 class Ui_LOGINFORM(object):
+
+    def showMessage(self, msgType, message):
+        messageBox = QtWidgets.QMessageBox()
+        messageBox.setIcon(QtWidgets.QMessageBox.Information)
+        messageBox.setWindowTitle(msgType)
+        messageBox.setText(message)
+        messageBox.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Close)
+        messageBox.exec_()
+
+    def signUpCheck(self):
+        print("Sign Up Button Clicked.")
+        self.SIGNUPFORM = QtWidgets.QDialog()
+        self.ui = Ui_SIGNUPFORM()
+        self.ui.setupUi(self.SIGNUPFORM)
+        self.SIGNUPFORM.show()
+
     def loginCheck(self):
         print("Log In Button Clicked.")
-    
+        self.showMessage("Information", 'Successfully logged in :)')
+
     def setupUi(self, LOGINFORM):
         LOGINFORM.setObjectName("LOGINFORM")
         LOGINFORM.resize(481, 341)
         LOGINFORM.setStyleSheet("QDialog{\n"
-"    background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0             rgba(154, 189, 149, 255), stop:1 rgba(255, 255, 255, 255));\n"
-"}\n"
-"\n"
-"QLineEdit{\n"
-"    background-color:rgb(239, 239, 179)\n"
-"}\n"
-"QPushButton#login{\n"
-"    background-color:rgb(0, 186, 0);\n"
-"    border:none;\n"
-"    color:rgb(0, 0, 67);\n"
-"}\n"
-"QPushButton#signUp{\n"
-"    background-color:rgb(245, 0, 0);\n"
-"    border:none;\n"
-"    color:rgb(226, 226, 0)\n"
-"}")
+                                "    background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0             rgba(154, 189, 149, 255), stop:1 rgba(255, 255, 255, 255));\n"
+                                "}\n"
+                                "\n"
+                                "QLineEdit{\n"
+                                "    background-color:rgb(239, 239, 179)\n"
+                                "}\n"
+                                "QPushButton#login{\n"
+                                "    background-color:rgb(0, 186, 0);\n"
+                                "    border:none;\n"
+                                "    color:rgb(0, 0, 67);\n"
+                                "}\n"
+                                "QPushButton#signUp{\n"
+                                "    background-color:rgb(245, 0, 0);\n"
+                                "    border:none;\n"
+                                "    color:rgb(226, 226, 0)\n"
+                                "}")
         self.log_form = QtWidgets.QLabel(LOGINFORM)
         self.log_form.setGeometry(QtCore.QRect(150, 30, 151, 41))
         font = QtGui.QFont()
@@ -78,11 +97,11 @@ class Ui_LOGINFORM(object):
         font.setWeight(75)
         self.login.setFont(font)
         self.login.setObjectName("login")
-        
+
         ############## Button Event#############
         self.login.clicked.connect(self.loginCheck)
         ############################################
-        
+
         self.signUp = QtWidgets.QPushButton(LOGINFORM)
         self.signUp.setGeometry(QtCore.QRect(150, 250, 101, 31))
         font = QtGui.QFont()
@@ -94,9 +113,9 @@ class Ui_LOGINFORM(object):
         self.signUp.setObjectName("signUp")
 
         ############## Button Event#############
-        self.signUp.clicked.connect(signUpCheck)
+        self.signUp.clicked.connect(self.signUpCheck)
         ############################################
-        
+
         self.line = QtWidgets.QFrame(LOGINFORM)
         self.line.setGeometry(QtCore.QRect(60, 80, 351, 16))
         self.line.setLineWidth(2)
@@ -109,7 +128,7 @@ class Ui_LOGINFORM(object):
 
     def retranslateUi(self, LOGINFORM):
         _translate = QtCore.QCoreApplication.translate
-        LOGINFORM.setWindowTitle(_translate("LOGINFORM", "Dialog"))
+        LOGINFORM.setWindowTitle(_translate("LOGINFORM", "Dialog","none"))
         self.log_form.setText(_translate("LOGINFORM", "Log In Form"))
         self.u_name.setText(_translate("LOGINFORM", "Username"))
         self.pass1.setText(_translate("LOGINFORM", "Password"))
@@ -119,6 +138,7 @@ class Ui_LOGINFORM(object):
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     LOGINFORM = QtWidgets.QDialog()
     ui = Ui_LOGINFORM()
