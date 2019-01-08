813a3e0d6679df732d2b8c0ced7ef53b13ed3e14
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-01-08 10:08:10 +0600

    Add files via upload

--- ImageShow/image.py
+++ ImageShow/image.py
@@ -1,33 +1,60 @@
 import sys
+from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QImage, QImageReader
+from PyQt5.QtWidgets import QVBoxLayout
 import mysql.connector
 #from PyQt5.QtCore import pyqtSlot
  
 class App(QWidget):
     
-    mydb = mysql.connector.connect(
-        host = 'localhost',
-        user = "root",
-        passwd = "ant904",
-        database = "imageDB"
-        #auth_plugin='mysql_native_password'
-    )
-    myCursor = mydb.cursor()
     file = ""
     curFileId = 1
 
     def showImage(self,filepath):
-        print(filepath)
-        self.label.hide()
         
-        self.label = QLabel(self)
+        self.label.clear()
+        print("from showImage function: ", filepath)
+        '''
+        #self.label = QLabel(self)
         self.pixmap = QPixmap(filepath)
         self.label.setPixmap(self.pixmap)
         self.label.setGeometry(50,20,550,400)
-        self.show()
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        
+        #self.initUI
+        #self.label1.show()
+        
+        self.label1 = QLabel(self)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.label1)
+        
+        #self.label1 = QLabel(self)
+        
+        image = QImage(QImageReader(filepath).read())
+        label1.setPixmap(QPixmap(image))
+        
+        pixmap = QPixmap(filepath)
+        label1.setPixmap(self.pixmap)
+        label1.setGeometry(50,20,550,400)
+        label1.show()
+        '''
+        pixmap=QtGui.QPixmap(filepath)
+        pixmap=pixmap.scaled(self.label.width(),
+            self.label.height(), QtCore.Qt.KeepAspectRatio)
+        self.label.setPixmap(pixmap)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        #print("doesn't show")
         
     def on_click_prev(self):
+        mydb = mysql.connector.connect(
+            host = 'localhost',
+            user = "root",
+            passwd = "ant904",
+            database = "imageDB"
+            #auth_plugin='mysql_native_password'
+        )
+        myCursor = mydb.cursor()
         #global curFileId
         if (App.curFileId-1) < 1:
             self.buttonP.hide()
@@ -37,13 +64,16 @@ class App(QWidget):
             sql = "SELECT Path FROM ImagePath where Image_ID = %s"
             val = (App.curFileId-1 ,)
             myCursor.execute(sql,val)
-            myresult = myCursor.fetchall()
-            for data in myresult:
-                print(data)
+            result = myCursor.fetchone()
+            for record in result:
+                print(record)
                 #file = record
-                self.showImage(data)
+                self.showImage(record)
                 #global curFileId
                 App.curFileId -= 1
+                record = ""
+        myCursor.close()
+        mydb.close()
                 
     def on_click_next(self):
         print("in next click")
@@ -55,11 +85,7 @@ class App(QWidget):
             #auth_plugin='mysql_native_password'
         )
         myCursor = mydb.cursor()
-        '''
-        sql = "SELECT Path FROM ImagePath where Image_ID = %d"
-        val = ((cid+1))
-        myCursor.execute(sql, val)
-        '''
+       
         myCursor.execute("SELECT Path FROM ImagePath")
         myresult = myCursor.fetchall()
         total = myCursor.rowcount
@@ -82,7 +108,10 @@ class App(QWidget):
                 self.showImage(data)
                 #global curFileId
                 App.curFileId += 1
-  
+                data = ""
+        myCursor.close()
+        mydb.close()
+        
     def __init__(self):
         super().__init__()
 
@@ -99,7 +128,7 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-     
+        
     # Image  widget
         self.label = QLabel(self)
         self.pixmap = QPixmap('F:\StudyMaterials\Python_pyQt5\Images\Green_Mango.jpg')
@@ -107,7 +136,7 @@ class App(QWidget):
         self.label.setGeometry(50,20,550,400)
         #self.resize(pixmap.width(),pixmap.height())
         #self.resize(640,450)
-
+        
     # Previous button widget
         self.buttonP = QPushButton('Previous', self)
         self.buttonP.setToolTip('Go to previous picture')
